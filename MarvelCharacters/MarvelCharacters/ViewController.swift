5092fb7daa3b099d1f29e19e680cedaef7fbcfc2
Ketan Parmar <ketan.engineer14@gmail.com>
2022-10-16 14:24:42 +0530

    project created

---
+++ MarvelCharacters/MarvelCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelCharacters
+//
+//  Created by Ketan Parmar on 16/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
